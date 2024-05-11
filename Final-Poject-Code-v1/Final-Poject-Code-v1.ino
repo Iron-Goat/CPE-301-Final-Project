b1493e71579ab57c0f963d3f518620974d6d2a62
1106-Mcginnis-Nathan <144670003+1106-Mcginnis-Nathan@users.noreply.github.com>
2024-05-10 22:14:53 -0700

    Update Final-Poject-Code-v1.ino
    Code for the clock function, still needs some start conditions however

--- Final-Poject-Code-v1/Final-Poject-Code-v1.ino
+++ Final-Poject-Code-v1/Final-Poject-Code-v1.ino
@@ -25,6 +25,9 @@
 #include <LiquidCrystal.h>
 //HAVE TO DECIDE WHAT PINS TO USE FOR LCD
 
+#include <RTClib.h>
+//Library for clock
+
 // UART Pointers
 volatile unsigned char *myUCSR0A = (unsigned char *)0x00C0; // <-- Lab 7,5 Values
 volatile unsigned char *myUCSR0B = (unsigned char *)0x00C1;
@@ -32,19 +35,24 @@ volatile unsigned char *myUCSR0C = (unsigned char *)0x00C2;
 volatile unsigned int  *myUBRR0  = (unsigned int *) 0x00C4;
 volatile unsigned char *myUDR0   = (unsigned char *)0x00C6;
 
+RTC_DS1307 rtc;
 
 void setup() {
-  // put your setup code here, to run once:
+
   
   // Start the UART
   U0Init(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
 
 //for display - change inputs when we figure that out
 void = Display(int a, int b);
+
+//for clock - change inputs once we figure out start button pointer
+void = Clock(int a);
+
 
 }
 
@@ -89,4 +97,26 @@ lcd.setCursor(0, 1);
 //Change if statement vairables once we decide on them
 if(WATER_LEVEL_VARIABLE < INTERUPT_SPECIFICATIONS){
 lcd.print("Water Level Low!");
+}}
+
+void Clock(int a){
+
+DateTime time = rct.now();
+//ADD CHANGES FOR VARIABLE ONCE WE FIGURE IT OUT!!
+//CHANGE START CONDITIONS SO THAT THIS RUNS ONCE PER STOP AND ONCE AGAIN PER START
+//if(theortical pointer = 1){
+Serial.printLn(Time);
+Serial.print(time.hour(), DEC);
+Serial.print(":");
+Serial.print(time.minute(), DEC);
+Serial.print(":");
+Serial.print(time.second(), DEC);
+Serial.printLn(Date);
+Serial.print(time.day(), DEC);
+Serial.print(":");
+Serial.print(time.month(), DEC);
+Serial.print(":");
+Serial.print(time.year(), DEC);
+//}
+
 }
